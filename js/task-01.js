c8eb2bb1519d6174c3b9912151ecb1ad58f1a919
Andrii Kurshatsov <andreswebit@ukr.net>
2023-07-01 17:08:36 +0300

    Task 1-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,37 @@
+//Кнопка "Приховати" ховає текст і замінює назву кнопки на
+//"Розкрити", при повторному натисканні текст знову стає доступним
+//і кнопка набуває початкового вигляду.
 
+const refs = {
+  taskTitle: document.querySelector(".taskTitle"),
+  passwordInput: document.querySelector("#passwordInput"),
+  passwordButton: document.querySelector("#passwordButton"),
+};
+
+refs.passwordButton.addEventListener("click", passwordButtonHandler);
+
+function passwordButtonHandler(event) {
+  if (refs.passwordButton.textContent === "Скрыть") {
+    refs.passwordButton.textContent = "Розкрити";
+    refs.taskTitle.style.display = "none";
+  } else {
+    refs.passwordButton.textContent = "Скрыть";
+    refs.taskTitle.style.display = "block";
+  }
+}
+
+//Натискання на кнопку "SHOW ME" має виводити значення
+//з поля введення(дивіться на елементи в html - розмітці)
+// const refs = {
+//   taskTitle: document.querySelector(".taskTitle"),
+//   alertInput: document.querySelector("#alertInput"),
+//   alertButton: document.querySelector("#alertButton"),
+// };
+
+// refs.alertButton.addEventListener("click", onAlertButtonClick);
+
+// function onAlertButtonClick(evt) {
+//   const inputValue = refs.alertInput.value;
+//   refs.taskTitle.textContent = inputValue;
+//   refs.alertInput.value = "";
+// }
